e730e1c8550639c866cd2fc898543f5852a01544
Simon Li <orpheus+devel@gmail.com>
2024-01-25 22:48:15 +0000

    Rewrite recipe

---
+++ recipe/build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -eux
+
+# Default LDFLAGS from conda environment break the build
+unset LDFLAGS
+
+TAGS="bindata sqlite sqlite_unlock_notify" make build
+
+mkdir -p $PREFIX/bin
+cp gitea $PREFIX/bin/gitea
+
+# Cross-compilation: check correct architecture was built
+if [ -n "$GOARCH" ]; then
+  if [ "$GOOS" = darwin ]; then
+    if [ "$GOARCH" = arm64 ]; then
+      PATTERN="Mach-O.+arm64"
+    else
+      PATTERN="Mach-O.+x86_64"
+    fi
+  else
+    if [ "$GOARCH" = arm64 ]; then
+      PATTERN="ELF.+aarch64"
+    else
+      PATTERN="ELF.+x86-64"
+    fi
+  fi
+
+  file $PREFIX/bin/gitea | grep -E "$PATTERN"
+fi
+
+# There are non-go files
+go-licenses save . --save_path=./license-files || true
+test -d license-files/github.com
